01790977f728d0b694a1939b832bb01bbce039e2
joeyceni <cenicola74@gmail.com>
2024-08-01 07:42:38 -0400

    update book reviews by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,21 +39,41 @@ public_users.get('/author/:authorName',function (req, res) {
         // res.json(filteredBooks);
         res.send(filteredBooks);
     } else {
-        res.status(404).json({ message: "No books found by this author" });
+        res.status(404).json({ message: "No books found by this author." });
     }
-    
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the title parameter from the request URL and send the corresponding books's details
+    // const keys = Object.keys(books);
+    const title = req.params.title;
+
+    // Get all book objects as an array
+    const bookArray = Object.values(books);
+
+    // Find books by the specified author
+    const filteredBooks = bookArray.filter(book => book.title.toLowerCase() === title.toLowerCase());
+
+    if (filteredBooks.length > 0) {
+        res.send(filteredBooks);
+    } else {
+        res.status(404).json({ message: "No books found with this title." });
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = parseInt(req.params.isbn);
+
+    if (books[isbn]) {
+        const book = books[isbn];
+
+        res.send(book.reviews)
+
+    } else {
+        res.status(404).json({ message: "No reviews found for this ISBN." });
+    }
 });
 
 module.exports.general = public_users;
